919a2ce02a349e11cf5476f9b479a74a72653912
yeonju <514tndud@naver.com>
2014-11-25 15:29:18 +0900

    1st commit

---
+++ test.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.get('/test', function (req, res) {
+  res.send('Hello test');
+});
+
+
+app.get('/html', function (req, res)
+	res.send('<h1>hello html</h1>');
+});
+
+
+app.listen(3000);
